de14d64e485cee53041f833dabfd45aa1ab52177
barriecheng <barrie.cheng@gmail.com>
2026-07-22 16:37:59 +0800

    Integrate Firebase messaging in service worker
    Updated service worker to include Firebase initialization and background message handling.

--- firebase-messaging-sw.js
+++ firebase-messaging-sw.js
@@ -1,16 +1,27 @@
-self.addEventListener('push', event => {
-  const data = event.data.json();
-  console.log('data', data);
+// firebase-messaging-sw.js
+importScripts('https://www.gstatic.com/firebasejs/11.7.1/firebase-app-compat.js');
+importScripts('https://www.gstatic.com/firebasejs/11.7.1/firebase-messaging-compat.js');
 
-  self.registration.showNotification(data.notification.title, {
-    body: data.notification.body,
-    icon: data.notification.image,
-    data: data.data
-  });
+firebase.initializeApp({
+  apiKey: "AIzaSyA9lJFHBl0RQ6joVHvGX4kOkTsgyWZtpYQ",
+  authDomain: "web-notification-459705.firebaseapp.com",
+  projectId: "web-notification-459705",
+  storageBucket: "web-notification-459705.firebasestorage.app",
+  messagingSenderId: "314033823953",
+  appId: "1:314033823953:web:c6a222651ab47ea48465f0",
+  measurementId: "G-G4XLB03BNN"
 });
 
-self.addEventListener('notificationclick', event => {
-  event.notification.close();
-  const url = event.notification.data?.url || '/';
-  event.waitUntil(clients.openWindow(url));
+const messaging = firebase.messaging();
+
+messaging.onBackgroundMessage((payload) => {
+  console.log('[firebase-messaging-sw.js] 背景收到推播:', payload);
+
+  const notificationTitle = payload.notification.title;
+  const notificationOptions = {
+    body: payload.notification.body,
+    icon: '/favicon.ico' // 可選：你的 App 圖示
+  };
+
+  self.registration.showNotification(notificationTitle, notificationOptions);
 });
